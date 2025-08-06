70148037932d68d35cf6dd22405ca7f8f047cba4
teostefan <174971170+teostefan@users.noreply.github.com>
2025-08-05 21:06:53 -0400

    Fixed firmware issues in the ATTINY45 microcontroller that resulted in I²C bus desynchronisation by using hardware PWM peripherals instead of emulating PWM through software.

--- software/esc/esc.ino
+++ software/esc/esc.ino
@@ -1,6 +1,6 @@
 /*********************************************************\
 |                                                         |
-| esc_firmware.ino                                        |
+| esc.ino                                                 |
 |                                                         |
 | © 2025 Oscar Barbieri and Stefan Teodorescu.            |
 |                                                         |
@@ -10,10 +10,13 @@
 | For proper compilation on the Arduino IDE, the          |
 | following setup has to be used:                         |
 |                                                         |
-| 1. Install the ATTiny Board Package by David A Mellis.  |
-| 2. Set board to "ATtiny25/45/85".                       |
-| 3. Set clock to "Internal 8MHz".                        |
-| 4. Use Arduino as ISP programmer.                       |
+| 1. Copy just this file into an empty directory named    |
+|    "esc" without the other C files found in the parent  |
+|    directory. Open the file using the Arduino IDE v2.   |
+| 2. Install the ATTiny Board Package by David A Mellis.  |
+| 3. Set board to "ATtiny25/45/85".                       |
+| 4. Set clock to "Internal 8MHz".                        |
+| 5. Use Arduino as ISP programmer.                       |
 |                                                         |
 | This program is small enough to be used on an ATTINY25. |
 |                                                         |
@@ -21,105 +24,50 @@
 
 #include <USIWire.h> // NOLINT: Ignore this warning as the code will be compiled with Arduino IDE.
 
+#define HI_NIBBLE(b) (((b) >> 4) & 0x0F) // Get upper nibble from byte. Ex.: 0x36 -> 0x03
+#define LO_NIBBLE(b) ((b) & 0x0F)        // Get lower nibble from byte. Ex.: 0x36 -> 0x06.
+#define DBL_NIBBLE(n) ((n << 4) | n)     // Duplicate a nibble to form a byte. Ex.: 0x03 -> 0x33.
+
 #define SLAVE_ADDRESS 0x08
 
 #define MOT_A 3
 #define MOT_B 4
-#define TACH 1
-
-#define PWM_FREQ 60 // Hz
-#define PWM_PERIOD 1000 / PWM_FREQ
-
-unsigned long tachometer_acc = 0;
-
-char MOT_A_when_pwm_high = HIGH;
-char MOT_A_when_pwm_low = HIGH;
-char MOT_B_when_pwm_high = HIGH;
-char MOT_B_when_pwm_low = HIGH;
-
-unsigned int time_off = PWM_PERIOD / 2;
 
 void setup() {
     pinMode(MOT_A, OUTPUT);
     pinMode(MOT_B, OUTPUT);
 
-    cli(); // Disable global interrupts.
-    PCMSK |= (1 << TACH);
-    GIMSK |= (1 << PCIE);
-    pinMode(TACH, INPUT_PULLUP);
-    sei(); // Enable global interrupts.
-
     Wire.begin(SLAVE_ADDRESS);
     Wire.onReceive(receiveData);
     Wire.onRequest(sendData);
+    // The next section was inspired from http://www.technoblogy.com/show?LE0=:
+    TCCR1 = 1 << CTC1 | 1 << PWM1A | 3 << COM1A0 | 7 << CS10;
+    GTCCR = 1 << PWM1B | 3 << COM1B0;
+    // Interrupts on OC1A match and overflow
+    TIMSK = TIMSK | 1 << OCIE1A | 1 << TOIE1;
 }
 
-void loop() { // A low frequancy software PWM routine for the H-bridge IC.
-    delay(time_off);
-    digitalWrite(MOT_A, MOT_A_when_pwm_high);
-    digitalWrite(MOT_B, MOT_B_when_pwm_high);
-    delay(PWM_PERIOD - time_off);
-    digitalWrite(MOT_A, MOT_A_when_pwm_low);
-    digitalWrite(MOT_B, MOT_B_when_pwm_low);
+ISR(TIMER1_COMPA_vect) {
+    if (!bitRead(TIFR, TOV1)) bitSet(PORTB, MOT_A);
 }
 
-ISR(INT0_vect) { // Event to handle a pulse has been recieved from the tachometer.
-    tachometer_acc++;
+ISR(TIMER1_OVF_vect) {
+    bitClear(PORTB, MOT_A);
 }
 
-void receiveData(int bytecount) { // Event when we recieve an I²C write.
-    if (bytecount <= 0 || bytecount > 2) return;
-
-    char cmd = Wire.read();                         // Read the command.
-    char val = bytecount == 2 ? Wire.read() : 0x00; // Did we get a value along with the command?
-
-    switch (cmd) {
-    case 0x80: // Set motor speed (out of 255).
-        time_off = PWM_PERIOD - PWM_PERIOD * val / 255;
-        break;
-
-    case 0x81: // Brake.
-        MOT_A_when_pwm_high = HIGH;
-        MOT_A_when_pwm_low = HIGH;
-        MOT_B_when_pwm_high = HIGH;
-        MOT_B_when_pwm_low = HIGH;
-        break;
-
-    case 0x82: // Turn motor forwards.
-        MOT_A_when_pwm_high = HIGH;
-        MOT_A_when_pwm_low = LOW;
-        MOT_B_when_pwm_high = LOW;
-        MOT_B_when_pwm_low = LOW;
-        break;
-
-    case 0x83: // Turn motor in reverse.
-        MOT_A_when_pwm_high = LOW;
-        MOT_A_when_pwm_low = LOW;
-        MOT_B_when_pwm_high = HIGH;
-        MOT_B_when_pwm_low = LOW;
-        break;
-
-    case 0x84: // Coast.
-        MOT_A_when_pwm_high = LOW;
-        MOT_A_when_pwm_low = LOW;
-        MOT_B_when_pwm_high = LOW;
-        MOT_B_when_pwm_low = LOW;
-        break;
-
-    case 0x85: // Reset tachometer.
-        tachometer_acc = 0;
-    }
+void loop() {
 }
 
-void sendData() { // If we try to read from any I²C address, just return the tachometer counter.
-    // Split tachometer counter into four bytes (little-endian; least significant byte first).
-    char output[4];
+void receiveData(int bytecount) { // Event when an I²C write is recieved.
+    // Control byte format is as follows: xxxxyyyy where xxxx and yyyy are nibbles from 0 to 15 representing
+    // the duty cycle for driving the H-bridge channels IN1 (xxxx) and IN2 (yyyy).
+    if (bytecount != 1) return;
 
-    output[0] = tachometer_acc & 0xFF;
-    output[1] = (tachometer_acc >> 8) & 0xFF;
-    output[2] = (tachometer_acc >> 16) & 0xFF;
-    output[3] = (tachometer_acc >> 24) & 0xFF;
+    char bytein = Wire.read();
+    OCR1A = DBL_NIBBLE(LO_NIBBLE(bytein));
+    OCR1B = DBL_NIBBLE(HI_NIBBLE(bytein));
+}
 
-    // Write to output.
-    Wire.write(output, sizeof(output));
+void sendData() { // There is no I²C registers to be read from.
+    Wire.write(0x00);
 }

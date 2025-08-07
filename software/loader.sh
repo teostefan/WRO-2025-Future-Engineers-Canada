14ac1454eabe52649c95e538a2a4fbb824492417
OscarBarbieri <oskie.barbieri@gmail.com>
2025-08-07 15:39:13 -0400

    Added polling routine for starting the main program

---
+++ software/loader.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+is_running=false
+debounce_time=0.5 # Seconds
+last_press_time=0
+
+while true; do
+    value=$(raspi-gpio get 17 | grep -o 'level=.' | cut -d= -f2)
+    current_time=$(date +%s)
+
+    if [ "$value" -eq 0 ]; then
+        # Check if enough time has passed since the last button press, for debouncing.
+        if (( $(echo "$current_time - $last_press_time > $debounce_time" | bc -l) )); then
+            if [ "$is_running" = false ]; then
+                echo "Button press detected; starting main program."
+                /home/wro/Documents/WRO-2025-Future-Engineers-Canada/builds/main &
+                is_running=true
+            else
+                echo "Button press detected; stopping main program."
+                pkill -f "/home/wro/Documents/WRO-2025-Future-Engineers-Canada/builds/main"
+                is_running=false
+            fi
+            last_press_time=$current_time
+        fi
+    fi
+
+    sleep 0.2 # This should help reduce processing load caused by this script...
+done
